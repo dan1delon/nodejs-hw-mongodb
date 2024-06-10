2e1b1cbb658f43774c2629c0f61e29302b00b8c2
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-06-10 15:47:55 +0300

    upd

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,10 +6,12 @@ import {
   updateContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getAllContactsController = async (req, res, next) => {
   try {
-    const contacts = await getAllContacts();
+    const { page, perPage } = parsePaginationParams(req.query);
+    const contacts = await getAllContacts({ page, perPage });
 
     res.status(200).json({
       status: '200',
